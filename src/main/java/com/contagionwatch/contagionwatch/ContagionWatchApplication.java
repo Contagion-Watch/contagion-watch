075e655e9a73d0ecdeb1d869e321187bb49e3792
colinmarchbanks <marchbanks.colin@gmail.com>
2020-02-07 11:47:06 -0600

    first commit

---
+++ src/main/java/com/contagionwatch/contagionwatch/ContagionWatchApplication.java
@@ -0,0 +1,13 @@
+package com.contagionwatch.contagionwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContagionWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContagionWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contagionwatch/contagionwatch/ContagionWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contagionwatch.contagionwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContagionWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
