f90f126f8db0155b3b981fbb38485a0e814fbc89
Joshua Peters <peters.l.joshua@gmail.com>
2020-02-07 12:13:13 -0600

    saving first page for deployment

---
+++ src/main/java/com/contagionwatch/contagionwatch/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.contagionwatch.contagionwatch.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String homePage(){
+        return "home";
+    }
+
+
+
+
+}
