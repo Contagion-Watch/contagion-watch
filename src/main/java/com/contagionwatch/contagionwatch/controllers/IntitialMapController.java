8ed5d0414537d53d27ced9bfbef4b60595e41c07
colinmarchbanks <marchbanks.colin@gmail.com>
2020-02-11 10:13:26 -0600

    added mapbox geocode and other maps

--- src/main/java/com/contagionwatch/contagionwatch/controllers/IntitialMapController.java
+++ src/main/java/com/contagionwatch/contagionwatch/controllers/IntitialMapController.java
@@ -1,6 +1,5 @@
 package com.contagionwatch.contagionwatch.controllers;
 
-import com.contagionwatch.contagionwatch.config.Keys;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IntitialMapController {
 
-    Keys key = new Keys();
     @GetMapping("/test-map")
-    public String mapGet(Model model){
-        model.addAttribute("key", key.getMapboxKey());
+    public String mapGet(){
         return "maps/intial-map";
     }
 }
