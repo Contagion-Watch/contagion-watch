93e9de673825c4c76d618a3b10ecd72e939bb15b
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2020-02-13 16:03:10 -0600

    disease info

--- src/main/java/com/contagionwatch/contagionwatch/controllers/InformationController.java
+++ src/main/java/com/contagionwatch/contagionwatch/controllers/InformationController.java
@@ -1,12 +1,22 @@
 package com.contagionwatch.contagionwatch.controllers;
 
+import com.contagionwatch.contagionwatch.dao.DiseaseRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class InformationController {
-    @GetMapping("/information")
-    public String informationPage(){
+    private final DiseaseRepository diseaseDao;
+
+    public InformationController(DiseaseRepository diseaseDao) {
+        this.diseaseDao = diseaseDao;
+    }
+
+    @RequestMapping(value = "/information/{id}", method = RequestMethod.GET)
+    public String diseaseInfo(@PathVariable Long id, Model model) {
+        model.addAttribute("disease", diseaseDao.getDiseaseById(id));
         return "information";
     }
+
 }
