fc1561be5ac1fbc1c5874bd599da338617e05a38
Joshua Peters <peters.l.joshua@gmail.com>
2020-02-15 13:12:18 -0600

    commit'n on the weekend

--- src/main/java/com/contagionwatch/contagionwatch/controllers/HomeController.java
+++ src/main/java/com/contagionwatch/contagionwatch/controllers/HomeController.java
@@ -1,23 +1,12 @@
 package com.contagionwatch.contagionwatch.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-    @Value("${mapboxKey}")
-    private String mapboxKey;
-
     @GetMapping("/home")
-    public String homePage(Model model){
-        model.addAttribute("mapboxKey",mapboxKey);
+    public String loginPage(){
         return "home";
     }
-
-
-
-
 }
