65f72ac2df2d7607443fa0210cd0f0861a786708
colinmarchbanks <marchbanks.colin@gmail.com>
2020-02-19 15:31:29 -0600

    small changes

--- src/main/java/com/contagionwatch/contagionwatch/controllers/InformationController.java
+++ src/main/java/com/contagionwatch/contagionwatch/controllers/InformationController.java
@@ -4,23 +4,12 @@ import com.contagionwatch.contagionwatch.dao.DiseaseRepository;
 import com.contagionwatch.contagionwatch.dao.EntryRepository;
 import com.contagionwatch.contagionwatch.dao.LocationRepository;
 import com.contagionwatch.contagionwatch.models.Entry;
-import com.mysql.cj.xdevapi.JsonArray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.json.*;
-import org.json.JSONException;
-
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
--- src/main/java/com/contagionwatch/contagionwatch/controllers/MapController.java
+++ src/main/java/com/contagionwatch/contagionwatch/controllers/MapController.java
@@ -4,13 +4,10 @@ import com.contagionwatch.contagionwatch.dao.DiseaseRepository;
 import com.contagionwatch.contagionwatch.dao.LocationRepository;
 import com.contagionwatch.contagionwatch.models.Entry;
 import com.contagionwatch.contagionwatch.models.Location;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
